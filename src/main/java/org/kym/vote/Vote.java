6a911962d97cd49c57bad3235fd65a0cb271c1d7
Kimyoungmin36 <pon36@naver.com>
2019-05-05 23:58:42 +0900

    first commit

---
+++ src/main/java/org/kym/vote/Vote.java
@@ -0,0 +1,40 @@
+package org.kym.vote;
+
+public class Vote {
+	String pollId;
+	String userId;
+	String name;
+	int choice;
+	
+	public String getPollId() {
+		return pollId;
+	}
+	public void setPollId(String pollId) {
+		this.pollId = pollId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getChoice() {
+		return choice;
+	}
+	public void setChoice(int choice) {
+		this.choice = choice;
+	}
+	@Override
+	public String toString() {
+		return "Vote [pollId=" + pollId + ", userId=" + userId + ", name=" + name + ", choice=" + choice + "]\n";
+	}
+	
+	
+}
+//
---
+++ src/main/java/org/kym/vote/VoteMain.java
@@ -0,0 +1,19 @@
+package org.kym.vote;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class VoteMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"vote-context.xml");
+		VoteService pollService = context.getBean("voteService",
+				VoteService.class);
+
+		pollService.voteFor();
+		pollService.listVotes();
+		context.close();
+	}
+
+}
